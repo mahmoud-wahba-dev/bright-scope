7f3c78daf6880ea9ea83ceef5d7451ab1cd4079a
mahmoud.wahba.dev <mahmoud.wahba.dev@gmail.com>
2025-10-10 00:42:26 +0300

    update

--- src/layout/MasterLayout.jsx
+++ src/layout/MasterLayout.jsx
@@ -1,8 +1,17 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const MasterLayout = () => {
+    const location = useLocation();
+
+  useEffect(() => {
+    // Ensure Flyon UI is reinitialized on each route change
+    if (window.HSStaticMethods && window.HSStaticMethods.autoInit) {
+      window.HSStaticMethods.autoInit();
+    }
+  }, [location.pathname]);
   return (
     <>
       <Navbar />
