c2f7efd20eb8b16bc660638a7a53f9e45f550d05
mahmoud.wahba.dev <mahmoud.wahba.dev@gmail.com>
2025-10-21 21:38:59 +0300

    update

--- src/pages/Home/components/OurServices.jsx
+++ src/pages/Home/components/OurServices.jsx
@@ -60,6 +60,7 @@ const OurServices = () => {
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
           {services.length > 0 ? (
+            Array.isArray(services) &&
             services.map((item) => (
               <div
                 key={item.id}
